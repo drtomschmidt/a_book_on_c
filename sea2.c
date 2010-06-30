797f3578880d96e2b7909f7e0f12adfe2eff158b
Matt Blair <me@matthewblair.net>
2010-06-29 20:52:27 -0400

    artsy

---
+++ sea2.c
@@ -0,0 +1,13 @@
+//I meant Kelley *&* Pohl in 3035d516e25cd2c04e7f. Duh.
+
+#include <stdio.h>
+
+int main(void) {
+    printf("\n\n\n\n\n\n\n\n\n\n"); //That's 10 newlines.
+    printf("            ***********************\n"); //That's 23 asterisks.
+    printf("            *  from sea           *\n");
+    printf("            *  to shining C       *\n");
+    printf("            ***********************\n");
+    printf("\n\n\n\n\n\n\n\n\n\n");
+    return 0;
+}
