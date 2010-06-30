6aea9d8f94ac385247727ff3055a7a8714e7f4db
Matt Blair <me@matthewblair.net>
2010-06-29 20:45:00 -0400

    working those newlines

--- sea.c
+++ sea.c
@@ -1,9 +1,10 @@
-//Regarding the now-missing return statement, Kelley Pohl had this to say:
-//"If we do not use it, the compiler will complain."
-//GCC 4.4 said nothing.
+//Same result.
 
 #include <stdio.h>
 
 int main(void) {
-    printf("from sea to shining C\n");
+    printf("from sea to ");
+    printf("shining C");
+    printf("\n");
+    return 0;
 }
