3035d516e25cd2c04e7f82af0bb603a3b155ee9a
Matt Blair <me@matthewblair.net>
2010-06-29 20:41:58 -0400

    seeing what removing the return statement would do

--- sea.c
+++ sea.c
@@ -1,6 +1,9 @@
+//Regarding the now-missing return statement, Kelley Pohl had this to say:
+//"If we do not use it, the compiler will complain."
+//GCC 4.4 said nothing.
+
 #include <stdio.h>
 
 int main(void) {
     printf("from sea to shining C\n");
-    return 0;
 }
