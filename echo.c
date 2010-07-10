4192288335a72e17c6a1507f976727f2b4ce647a
Matt Blair <me@matthewblair.net>
2010-07-10 16:35:42 -0400

    flow of control

--- echo.c
+++ echo.c
@@ -17,7 +17,14 @@ int main(void) {
 
     printf("\n%s\n%s", "Input three characters,", "an int, a float, and a double: \n");
     scanf("%c%c%c%d%f%lf", &c1, &c2, &c3, &i, &x, &y);
-    printf("\nHere is the data that you typed in:\n");
-    printf("%3c%3c%3c%5d%17e%17e\n\n", c1, c2, c3, i, x, y);
-    return 0;
+
+    if (c1 == 'A') {
+        printf("\nHere is the data that you typed in:\n");
+        printf("%3c%3c%3c%5d%17e%17e\n\n", c1, c2, c3, i, x, y);
+        return 0;
+    }
+
+    else {
+        return 2;
+    }
 }
